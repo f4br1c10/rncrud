0e0be0ffb990cedbb0d6187db2617c776572aefc
f4br1c10 <fabriciopstos@hotmail.com>
2021-06-05 17:46:26 -0300

    Lista de Usuários #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
-import { ListItem } from 'react-native-elements'
+import { View, FlatList, Image, StyleSheet, Alert } from 'react-native'
+import { ListItem, Button, Icon } from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
 
     function getUserItem({ item: user }) {
+
+        function confirmUserDeletion(user) {
+            Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+                {
+                    text: 'Sim',
+                    onPress() {
+                        console.warn('delete ' + user.id)
+                    }
+                },
+                {
+                    text: 'Não'
+                }
+            ])
+        }
+
         return (
             <ListItem
                 key={user.id}
@@ -17,6 +32,20 @@ export default props => {
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
+                <View style={styles.actionsButtons}>
+                    <Button
+                        onPress={() => {
+                            props.navigation.navigate('UserForm', user);
+                        }}
+                        type="clear"
+                        icon={<Icon name="edit" size={25} color="orange" />}
+                    />
+                    <Button
+                        onPress={() => { confirmUserDeletion(user) }}
+                        type="clear"
+                        icon={<Icon name="delete" size={25} color="red" />}
+                    />
+                </View>
             </ListItem>
         )
     }
@@ -37,5 +66,8 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         height: 40,
         width: 40
+    },
+    actionsButtons: {
+        flexDirection: 'row'
     }
 })
