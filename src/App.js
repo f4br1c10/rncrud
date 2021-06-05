8825ce27939f522c3ab159d99d9b9aa1557a7ffb
f4br1c10 <fabriciopstos@hotmail.com>
2021-06-05 14:51:42 -0300

    Navegação Stack #02

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
+import { Button, Icon } from 'react-native-elements'
 
 const Stack = createStackNavigator()
 
@@ -11,16 +12,43 @@ export default props = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName='UserList'>
+                initialRouteName='UserList'
+                screenOptions={screenOptions}>
                 <Stack.Screen
                     name='UserList'
                     component={UserList}
+                    options={({ navigation }) => {
+                        return {
+                            title: 'Lista de Usuários',
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => navigation.navigate('UserForm')}
+                                    type='clear'
+                                    icon={<Icon name='add' size={25} color='white' />}
+                                />
+                            )
+                        }
+                    }}
                 />
                 <Stack.Screen
                     name='UserForm'
                     component={UserForm}
+                    options={{
+                        title: 'Formulário de Usuários'
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e',
+
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
 }
