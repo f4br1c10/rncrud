fcfa6654fc20030c8471bb1854d249f1de1765e6
f4br1c10 <fabriciopstos@hotmail.com>
2021-06-05 16:18:55 -0300

    Lista de Usuários #01

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,41 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import users from '../data/users'
 
-export default props = () => {
+export default props => {
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                key={user.id}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm')}
+            >
+                <Image source={{ uri: user.avatarUrl }} style={styles.image} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        borderRadius: 20,
+        height: 40,
+        width: 40
+    }
+})
