5441bbb0410fe1f5fa752ceb440f7e8c269aa5e8
f4br1c10 <fabriciopstos@hotmail.com>
2021-06-05 15:01:24 -0300

    Criando Arquivo de Usuários

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: '1',
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id: '2',
+        name: 'Mariana Aguiar',
+        email: 'riamasil@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: '3',
+        name: 'Julio Fragoso',
+        email: 'juliafral@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'
+    },
+    {
+        id: '4',
+        name: 'Rafael Monteiro',
+        email: 'rafamontrr@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+    }
+]
